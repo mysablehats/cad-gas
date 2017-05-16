594f8aa6c55e85acb21e283047ee53aea75e5763
mysablehats <mysablehats@gmail.com>
2017-05-16 16:40:32 +0100

    what?
    this worked too well. i need to test it for the whole dataset as it
    might be a spur result

--- starter_script.m
+++ starter_script.m
@@ -74,7 +74,7 @@ else
 end
 simvar.NODES_VECT = [100];
 simvar.MAX_EPOCHS_VECT = [1];
-simvar.ARCH_VECT = [13];
+simvar.ARCH_VECT = [12];
 simvar.MAX_NUM_TRIALS = 1;
 simvar.MAX_RUNNING_TIME = 1;%3600*10; %%% in seconds, will stop after this
 
