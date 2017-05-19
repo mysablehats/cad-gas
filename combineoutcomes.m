ee046787de1cf52eb0bbaf4b2d6a09b1d0645595
mysablehats <mysablehats@gmail.com>
2017-05-19 18:25:36 +0100

    removed blackpc

--- combineoutcomes.m
+++ combineoutcomes.m
@@ -1,13 +1,13 @@
 function aaa = combineoutcomes
 env = aa_environment;
 try
-    for i = 1:3
+    for i = 1:2
         outcomes(i) = load([env.allmatpath 'outcomes' env.SLASH env.currhash '-outcomes-' num2str(i)] );
     end
 catch
     return
 end
-for i=1:3
+for i=1:2
     for j = outcomes(i).outcomes.idxs
         combineoutcomes(j) = outcomes(i).outcomes.b(j);
     end
