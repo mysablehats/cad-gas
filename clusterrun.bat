53197e1bf372ad229c831a2812c765b17aec31ba
mysablehats <mysablehats@users.noreply.github.com>
2017-05-22 14:26:07 +0100

    avalanche stuff

---
+++ clusterrun.bat
@@ -0,0 +1,3 @@
+@echo off
+C:\Users\fred\AppData\Local\GitHubDesktop\app-0.5.4\resources\app\git\cmd\git.exe pull
+matlab -nodisplay -nosplash -nodesktop -r "run('C:\Users\fred\Documents\GitHub\cad-gas\starter_script.m');exit"
--- test_cluster_starter.m
+++ test_cluster_starter.m
@@ -1,5 +1,6 @@
 %test cluster primitive
 env = aa_environment;
 pcid = 1;
-idxs = 1:2;
+
+idxs = 1:7;%16:43;
 save([env.homepath env.SLASH '..' env.SLASH 'clust.mat'],'pcid','idxs')
