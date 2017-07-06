a2d9ff246e50bbaa0edf59dc50278214634e4a9d
mysablehats <frederico.klein@gmail.com>
2017-07-06 11:32:35 +0100

    test trial

--- starter_script.m
+++ starter_script.m
@@ -95,8 +95,8 @@ if simvar.PARA
 else
     simvar.P = 1;
 end
-simvar.NODES_VECT = [1000];
-simvar.MAX_EPOCHS_VECT = [10];
+simvar.NODES_VECT = [3];
+simvar.MAX_EPOCHS_VECT = [1];
 simvar.ARCH_VECT = [1];
 simvar.MAX_NUM_TRIALS = 1;
 simvar.MAX_RUNNING_TIME = 1;%3600*10; %%% in seconds, will stop after this
