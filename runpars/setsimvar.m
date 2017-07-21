17175984d3c96923d8dc3bf7baa32aad34ebc589
mysablehats <frederico.klein@gmail.com>
2017-07-21 15:58:03 +0100

    nt

--- runpars/setsimvar.m
+++ runpars/setsimvar.m
@@ -45,7 +45,7 @@ end
 
 simvar.NODES_VECT = [100];
 simvar.MAX_EPOCHS_VECT = [1];
-simvar.ARCH_VECT = [1];
+simvar.ARCH_VECT = [8];
 
 simvar.MAX_NUM_TRIALS = 1;
 simvar.MAX_RUNNING_TIME = 1;%3600*10; %%% in seconds, will stop after this
