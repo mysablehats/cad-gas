5fc88586528f812ca8b35ef03a26b5d3709a10b7
mysablehats <frederico.klein@gmail.com>
2017-11-01 10:47:34 +0000

    nt

--- runpars/setdatavar.m
+++ runpars/setdatavar.m
@@ -10,7 +10,7 @@ datavar.validationtype = 'type2all'; %'type2notrandom'; 'cluster' 'quarterset' '
 if isempty(varargin)
     datavar.AllSubjects = [1 2 3 4];%2 %[1 2 3 4]; %% 
     datavar.disablesconformskel = 0;
-    datavar.generatenewdataset = false; %true;
+    datavar.generatenewdataset = true; %true;
     datavar.datasettype = 'CAD60'; % datasettypes are 'CAD60', 'tstv2' and 'stickman'
     datavar.activity_type = 'act_type'; %'act_type' or 'act'
     datavar.prefilter = {'none', 15};%{'filter',10}; % 'filter', 'none', 'median?'
@@ -19,7 +19,7 @@ if isempty(varargin)
     datavar.affrepvel = false;
     datavar.randSubjEachIteration = false; %%% must be set to false for systematic testing
     datavar.extract = {'rand', 'wantvelocity','order',{'removeaction','still','random'}};
-    datavar.preconditions =  {'nohips','mirrorx'};%{'nohips','polarC'};%'disthips', 'nonmatrixkilldim'};%, 'mirrorx'};% {'nohips'};% {'nohips', 'mirrorz', 'mirrorx'}; %,'normal'};%{'nohips', 'norotatehips' ,'mirrorx'}; %,
+    datavar.preconditions =  {'nohips','mirrorx','polarC'};%{'nohips','polarC'};%'disthips', 'nonmatrixkilldim'};%, 'mirrorx'};% {'nohips'};% {'nohips', 'mirrorz', 'mirrorx'}; %,'normal'};%{'nohips', 'norotatehips' ,'mirrorx'}; %,
 
 else
     datavar.featuresall = 3;%size(varargin{1},2);
